466438808431a1caafeac22c13a35768a8df4090
Andrey Kovalevskiy <Antoniodemagianis@yandex.ru>
2020-08-08 11:41:07 +0300

    lesson 10

---
+++ Homework/Task 10/script.js
@@ -0,0 +1,26 @@
+var cat = {
+    name: 'nameCat',
+    numOfLegs: 4,
+    food: 'foodCat',
+    age: 'ageCat',
+    color: 'colorCat',
+    eye: 'eyeCat'
+};
+
+var dog = {
+    name: 'nameDog',
+    numOfLegs: 4,
+    food: 'foodDog',
+    age: 'ageDog',
+    color: 'colorDog',
+    eye: 'eyeDog'
+};
+
+var bird = {
+    name: 'nameBird',
+    numOfLegs: 2,
+    food: 'foodBird',
+    age: 'ageBird',
+    color: 'colorBird',
+    eye: 'eyeBird'
+};
