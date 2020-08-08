7763519959687b3b9d809805b7484ffada5d509c
Andrey Kovalevskiy <Antoniodemagianis@yandex.ru>
2020-08-08 11:40:56 +0300

    lesson 9

---
+++ Homework/Task 9/script.js
@@ -0,0 +1,15 @@
+var number = 3;
+for (;number > 0; number--) {
+    document.write('Number > 0 <br>');
+}
+document.write('Number <= 0 <br>');
+
+var apples = 5;
+for (;apples > 0; apples--) {
+    document.write('Отдал яблоко другу<br>');
+}
+document.write('Яблок нет<br>');
+for (;apples < 10; apples++) {
+    document.write('Дай яблоко<br>');
+}
+document.write('Больше не надо<br>');
